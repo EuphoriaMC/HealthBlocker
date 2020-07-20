aff9056b8a4697d1cd892084c2ff3dbe9957606c
wsman217 <westonsublett1@gmail.com>
2020-07-20 14:59:34 -0500

    Fixed a spelling mistake in the README and changed CategoryView to use a hopper inventory.

--- src/main/java/me/wsman217/healthblocker/gui/CategoryView.java
+++ src/main/java/me/wsman217/healthblocker/gui/CategoryView.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
@@ -64,13 +65,13 @@ public class CategoryView implements Listener {
     public void openInv() {
         //Create the inventory and add the items to it
         //Inventory inv = Bukkit.createInventory(new MainHolder(), 9, INV_NAME);
-        Inventory inv = Bukkit.createInventory(new MainHolder(), 9, INV_NAME);
+        Inventory inv = Bukkit.createInventory(new MainHolder(), InventoryType.HOPPER, INV_NAME);
         /*inv.setItem(2, tier1Item);
         inv.setItem(4, tier2Item);
         inv.setItem(6, tier3Item);*/
         inv.setItem(0, tier1Item);
-        inv.setItem(4, tier2Item);
-        inv.setItem(8, tier3Item);
+        inv.setItem(2, tier2Item);
+        inv.setItem(4, tier3Item);
         p.openInventory(inv);
     }
 
